b24cbd8da58e152530ca535ac37ffdd33d84a996
Shawn Stephen Bradford <sbradfo5@hydra21.eecs.utk.edu>
2016-01-14 22:00:11 -0500

    Overall look and feel established. Next up: signing in and out.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('includes/template.php');
+
+start_template();
+
+?>
+
+<div class="jumbotron text-center">
+	<h1>QuizMe</h1>
+	<p>Created by students, to help students succeed.</p>
+</div>
+
+<div class="container-fluid bg-1">
+	<h1>A Topic</h1>
+	<h4>Some notes about the topic</h4>
+	<p>More Details</p>
+</div>
+
+<div class="container-fluid bg-2">
+	<h1>A Topic</h1>
+	<h4>Some notes about the topic</h4>
+	<p>More Details</p>
+</div>
+
+<?php
+
+
+end_template();
+
+?>
